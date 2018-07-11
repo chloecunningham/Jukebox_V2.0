49e898e87b4434e565eb517b3f98497aee55d80f
chloecunningham <34134134+chloecunningham@users.noreply.github.com>
2018-07-11 23:18:10 +0100

    Added the 2d array
    I have manually created a 2d array just in case there are issues with reading from the text file.

--- Visual_Studio_Files/Jukebox_v2.0/Jukebox_v2.0/Form1.cs
+++ Visual_Studio_Files/Jukebox_v2.0/Jukebox_v2.0/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
+using System.IO; //so the program can read and write to the disk.
 
 namespace Jukebox_v2._0
 {
@@ -21,25 +21,38 @@ namespace Jukebox_v2._0
         private void Jukebox_Load(object sender, EventArgs e)
 
         {
-            string path = @"C:\Users\Chloe\Desktop\Jukebox ReSub\Jukebox_V2.0\songs.txt"; //path to read the songs from
-            
+            string path = @"C:\Users\Chloe\Desktop\Jukebox ReSub\Jukebox_V2.0\songs.txt"; //path to read the songs from.
+
+            string[,] songs = new string[4,5] //2d array containing song names.
+            {
+                {
+                   "Parody", "Ducked_up_Lips", "Fallen_Kingdom", "Kim_Jong_Style", "On_the_Down_Low"
+                },
+                {
+                    "Pop", "If_I_Had_You", "Shut_Up", "Sleep_Walker", "Whataya_Want_From_Me"
+                },
+                {
+                    "Punk", "American_Idiot", "Burnout", "Give_Me_Novacaine", "Welcome_To_Paradise"
+                },
+                {
+                    "Rock", "Cut_the_Cord", "Miracle", "Nowhere_Kids", "Second_Chance"
+                }
+            };
 
         }
-
         
-
         private void label1_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void setToolStripMenuItem_Click(object sender, EventArgs e) //opens the set up box
+        private void setToolStripMenuItem_Click(object sender, EventArgs e) //opens the set up box.
         {
             Set_up f1 = new Set_up();
             f1.ShowDialog();
         }
 
-        private void aboutToolStripMenuItem_Click(object sender, EventArgs e) //opens the about page
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e) //opens the about page.
         {
             About f2 = new About();
             f2.ShowDialog();
